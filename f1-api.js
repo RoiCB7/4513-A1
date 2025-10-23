fbfbfa45fad0f235fea1892021ac69f329697334
RoiCB7 <cbbcalgary@hotmail.com>
2025-10-22 23:41:59 -0600

    Update f1-api.js

--- f1-api.js
+++ f1-api.js
@@ -83,8 +83,14 @@ app.use(function (req, res) {
   res.status(404).json({ error: "URL not found" });
 });
 
-let port = 8080;
+//let port = 8080;
+//app.listen(port, () => {
+ // console.log("Server running at port= " + port);
+//  console.log("http://localhost:8080/");
+//});
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log("Server running at port= " + port);
-  console.log("http://localhost:8080/");
+  console.log(`Server running on port ${port}`);
+  console.log(`http://localhost:${port}/`);
 });
